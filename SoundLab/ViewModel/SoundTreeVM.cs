b6def082c78157e98448226307fa146893098b96
Pahalin4ik <96795312+Pahalin4ik@users.noreply.github.com>
2023-01-17 20:49:18 +0200

    Add files via upload

---
+++ SoundLab/ViewModel/SoundTreeVM.cs
@@ -0,0 +1,39 @@
+﻿using SoundLab.Model;
+using SoundLab.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundLab.ViewModel
+{
+    public class SoundTreeVM: ViewModelBase
+    {
+        public MainVM Parent { get; set; }
+        public SoundModel Model { get; set; }
+        public SoundTreeVM(MainVM Parent)
+        {
+            this.Parent = Parent;
+            Model = Parent.Model;
+        }
+        public ObservableCollection<FileInfo> Files
+        {
+            get { return Model.Files; }
+        }
+        private RelayCommand _setFile;
+        public RelayCommand SetFile
+        {
+            get
+            {
+                return _setFile ??
+                  (_setFile = new RelayCommand(obj =>
+                  {
+                      Model.selectedFile = obj as string;
+                  }));
+            }
+        }
+    }
+}
